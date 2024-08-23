2bf0c428adba95f15a3dd4f98c851bb001836d02
Carlo Dandan <carlodandan.personal@proton.me>
2024-08-23 12:48:38 +0000

    Import SM-X115N_KOR Android 14 kernel platform.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

---
+++ build_kernel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/carlodandan/android_kernel_samsung_gta9.git -b master ./kernel-5.10
+cd kernel-5.10
+python scripts/gen_build_config.py --kernel-defconfig gta9_00_defconfig --kernel-defconfig-overlays "entry_level.config ot11.config" -m user -o ../out/target/product/gta9/obj/KERNEL_OBJ/build.config
+
+export ARCH=arm64
+export PLATFORM_VERSION=13
+export CROSS_COMPILE="aarch64-linux-gnu-"
+export CROSS_COMPILE_COMPAT="arm-linux-gnueabi-"
+export OUT_DIR="../out/target/product/gta9/obj/KERNEL_OBJ"
+export DIST_DIR="../out/target/product/gta9/obj/KERNEL_OBJ"
+export BUILD_CONFIG="../out/target/product/gta9/obj/KERNEL_OBJ/build.config"
+
+cd ../kernel
+./build/build.sh
