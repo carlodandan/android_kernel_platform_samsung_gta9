362b09f4b3a03199c3c73d19316a8d8091f64211
Carlo Dandan <carlodandan.personal@proton.me>
2024-08-25 11:48:28 +0000

    kernel: Revert changes.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,21 +2,21 @@
 
 sudo apt-get install bison flex rsync device-tree-compiler bc cpio lz4 -y
 
-git clone --depth=1 https://github.com/carlodandan/android_kernel_samsung_gta9.git -b master ./kernel_platform/kernel-5.10
-cd kernel_platform/kernel-5.10
-python scripts/gen_build_config.py --kernel-defconfig gta9_00_defconfig --kernel-defconfig-overlays "entry_level.config ot11.config" -m user -o ../build.config
+git clone --depth=1 https://github.com/carlodandan/android_kernel_samsung_gta9.git -b android14 ./kernel-5.10
 
-cd ..
+cd kernel-5.10
+python scripts/gen_build_config.py --kernel-defconfig gta9_00_defconfig --kernel-defconfig-overlays "entry_level.config ot11.config" -m user -o ../out/target/product/gta9/obj/KERNEL_OBJ/build.config
 
 export ARCH=arm64
 export PLATFORM_VERSION=13
 export CROSS_COMPILE="aarch64-linux-gnu-"
 export CROSS_COMPILE_COMPAT="arm-linux-gnueabi-"
-export OUT_DIR="out"
-export DIST_DIR="out"
-export BUILD_CONFIG="build.config"
-export CLANG_PREBUILT_BIN="prebuilts-master/clang/host/linux-x86/clang-r416183b/bin"
-export LD_LIBRARY_PATH="prebuilts-master/clang/host/linux-x86/clang-r416183b/lib64"
-export LINUX_GCC_CROSS_COMPILE_PREBUILTS_BIN="prebuilts/gas/linux-x86"
+export OUT_DIR="../out/target/product/gta9/obj/KERNEL_OBJ/build.config"
+export DIST_DIR="../out/target/product/gta9/obj/KERNEL_OBJ/build.config"
+export BUILD_CONFIG="../out/target/product/gta9/obj/KERNEL_OBJ/build.config"
+export CLANG_PREBUILT_BIN="../kernel/prebuilts-master/clang/host/linux-x86/clang-r416183b/bin"
+export LD_LIBRARY_PATH="../kernel/prebuilts-master/clang/host/linux-x86/clang-r416183b/lib64"
+export LINUX_GCC_CROSS_COMPILE_PREBUILTS_BIN="../kernel/prebuilts/gas/linux-x86"
 
+cd ../kernel
 ./build/build.sh 
